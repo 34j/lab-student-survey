16393762d92e82aa3f7672ffd8f89b219fcaeb12
34j <55338215+34j@users.noreply.github.com>
2023-09-21 14:14:40 +0900

    fix: fix misspelling

--- src/lab_student_survey/cli.py
+++ src/lab_student_survey/cli.py
@@ -11,6 +11,6 @@ from .main import main
 def cli(file_url: str | None = None) -> None:
     basicConfig(level=INFO)
     if file_url is None:
-        file_url = os.environ["LABO_STUDENT_SURVEY_FILE_URL"]
-    main(file_url, folder_url=os.environ.get("LABO_STUDENT_SURVEY_FOLDER_URL"))
+        file_url = os.environ["LAB_STUDENT_SURVEY_FILE_URL"]
+    main(file_url, folder_url=os.environ.get("LAB_STUDENT_SURVEY_FOLDER_URL"))
     click.echo("Output saved to output.html")
