7d2663fab639dc1efb312c89fb71fd5a1e254e4c
34j <55338215+34j@users.noreply.github.com>
2023-09-21 14:50:22 +0900

    test: update test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,5 @@
+import re
+
 from click.testing import CliRunner
 
 from lab_student_survey.cli import cli
@@ -6,4 +8,10 @@ from lab_student_survey.cli import cli
 def test_main() -> None:
     runner = CliRunner()
     result = runner.invoke(cli)
-    assert result.exit_code == 0
+    try:
+        assert result.exit_code == 0
+    except AssertionError:
+        # hide URL in error message
+        raise AssertionError(
+            re.sub(r"https://[^\s]+", "https://...", result.output)
+        ) from None
